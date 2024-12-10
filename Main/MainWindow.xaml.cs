558fc8b69e27c9693ac88a592ff600fdb8a56883
Maethan Tordeux <Maethan.Tordeux@etu.univ-smb.fr>
2024-12-10 16:33:56 +0100

    Deplacementssss

--- Main/MainWindow.xaml.cs
+++ Main/MainWindow.xaml.cs
@@ -1,147 +1,112 @@
-﻿using System.Text;
+﻿using System;
+using System.Security.AccessControl;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using System.Security.AccessControl;
-
-
 
 namespace Main
 {
-    
     public partial class MainWindow : Window
     {
-
-
-
         // ----------- CONSTANTES --------------------------------
-
-        // ---- CHEMINS
         private readonly string imagePath = "P:\\Sàé1.01\\CastleTrigger\\Assets\\Map_V1.png"; 
+        private Image fondMap;
+        public static readonly int CASE = 48;
+
 
 
         // ----------- Déclarations des variables ----------------
         private DispatcherTimer TimerJeu = new DispatcherTimer(); // C'est le chrono
-        private bool UpKeyPressed, DownKeyPressed, LeftKeyPressed, RightKeyPressed;
+        private bool Commande_Haut = false, Commande_Bas = false, Commande_Gauche = false, Commande_Droit = false;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        private void KeyBoardUp(object sender, KeyEventArgs e)
+        // ---- FENETRE
+        Canvas FenetreJeu = new Canvas();
+        Rectangle JOUEUR = new Rectangle
         {
-            if (e.Key == Key.Z)
-            {
-                Commande_Haut = true;
-            }
-            if (e.Key == Key.S)
-            {
-                Commande_Bas = true;
-            }
-            if (e.Key == Key.Q)
-            {
-                Commande_Gauche = true;
-            }
-            if (e.Key == Key.D)
-            {
-                Commande_Droit = true;
-            }
-        }
-
-        private void Window_KeyUp(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Z)
-            {
-                Commande_Haut = false;
-            }
-            if (e.Key == Key.S)
-            {
-                Commande_Bas = false;
-            }
-            if (e.Key == Key.Q)
-            {
-                Commande_Gauche = false;
-            }
-            if (e.Key == Key.D)
-            {
-                Commande_Droit = false;
-            }
-        }
+            Width = 48,
+            Height = 48,
+            Fill = Brushes.Red
+        };
 
         public MainWindow()
         {
             InitializeComponent();
-            FenetreJeu.Focus();
+            this.Content = FenetreJeu;
 
-            TimerJeu.Interval = TimeSpan.FromMilliseconds(16); // 60fps
+
+            TimerJeu.Interval = TimeSpan.FromMilliseconds(16);
             TimerJeu.Tick += JeuTic;
             TimerJeu.Start();
 
-            
 
-            // Création du fond d'écran
-            ImageBrush backgroundBrush = new ImageBrush();
-            backgroundBrush.ImageSource = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
-            
-
-            // Assigner l'ImageBrush comme fond du Grid
-            FenetreJeu.Background = backgroundBrush;
+            fondMap = new Image();
+            fondMap.Source = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+            FenetreJeu.Children.Add(fondMap); // Fond
+            Canvas.SetTop(fondMap, 0);
+            Canvas.SetLeft(fondMap, 0);
 
 
+            FenetreJeu.Children.Add(JOUEUR); // Joueur
+            Canvas.SetTop(JOUEUR, FenetreJeu.ActualHeight / 2);
+            Canvas.SetLeft(JOUEUR, FenetreJeu.ActualWidth / 2);
         }
 
-        private void JeuTic(object sender, EventArgs e) // sender permet de localiser qui l'a déclenché [la méthode]
-            /*
-             * La Méthode s'exécute 60x / secondes
-             */
+        private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            Console.WriteLine(Commande_Bas);
-            Deplacement();
-
+            if (e.Key == Key.Z) Commande_Haut = true;
+            if (e.Key == Key.S) Commande_Bas = true;
+            if (e.Key == Key.Q) Commande_Gauche = true;
+            if (e.Key == Key.D) Commande_Droit = true;
         }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Z) Commande_Haut = false;
+            if (e.Key == Key.S) Commande_Bas = false;
+            if (e.Key == Key.Q) Commande_Gauche = false;
+            if (e.Key == Key.D) Commande_Droit = false;
+        }
+
+        private void JeuTic(object sender, EventArgs e)
+        {
+
+            Canvas.SetTop(JOUEUR, FenetreJeu.ActualHeight / 2 - JOUEUR.Height / 2);
+            Canvas.SetLeft(JOUEUR, FenetreJeu.ActualWidth / 2 - JOUEUR.Width / 2);
+            Deplacement();
+        }
+
         public void Deplacement()
         {
-            double POS_TOP_JOUEUR=Canvas.GetTop(JOUEUR);
-            double POS_GAUCHE_JOUEUR = Canvas.GetLeft(JOUEUR);
-            if (Commande_Bas==true)
+            double fondMapX = Canvas.GetLeft(fondMap);
+            double fondMapY = Canvas.GetTop(fondMap);
+
+
+            if (Commande_Bas)
             {
-                Canvas.SetTop(JOUEUR, POS_TOP_JOUEUR+Constante_Deplacement);
-            }
-            if (Commande_Haut == true)
-            {
-                Canvas.SetTop(JOUEUR, POS_TOP_JOUEUR-Constante_Deplacement);
-            }
-            if (Commande_Gauche == true)
-            {
-                Canvas.SetLeft(JOUEUR, POS_GAUCHE_JOUEUR-Constante_Deplacement);
-            }
-            if (Commande_Droit == true)
-            {
-                Canvas.SetLeft(JOUEUR, POS_GAUCHE_JOUEUR+Constante_Deplacement);
+                Canvas.SetTop(fondMap, fondMapY - 10);
             }
 
+
+            if (Commande_Haut)
+            {
+                Canvas.SetTop(fondMap, fondMapY + 10);
+            }
+
+
+            if (Commande_Gauche)
+            {
+                Canvas.SetLeft(fondMap, fondMapX + 10);
+            }
+
+
+            if (Commande_Droit)
+            {
+                Canvas.SetLeft(fondMap, fondMapX - 10);
+            }
         }
-
     }
 }
