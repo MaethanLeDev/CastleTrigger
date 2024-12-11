479e26b9034d3d5c9b7eff22da02667aab698fa3
ArthurCODEBOSS* <Arthur.Lopes@etu.univ-smb.fr>
2024-12-11 16:38:06 +0100

    COLLISION PUSH

---
+++ Main/Constantes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public static class Constantes
+    {
+        public const double VITESSEJOUEUR = 10;
+        public static readonly int CASE = 48;
+    }
+}
